917d6622d2832fdccd439da89e434ce3e998bef5
Abhishek Shukla <37998474+abhi107@users.noreply.github.com>
2023-10-14 07:44:29 +0000

    Data types

---
+++ .devcontainer/basics/dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all js as newer version
+
+let name = "Samuel"
+let age = 24
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value(type = Object)
+// undefined => value not assigned
+// symbol => unique
+
+
+//object
+
+console.log(typeof null) // object
