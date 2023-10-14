d36e39fe675962bc05fd314714d63e96ca39ec35
Abhishek Shukla <37998474+abhi107@users.noreply.github.com>
2023-10-14 08:21:46 +0000

    datatype summary

---
+++ .devcontainer/basics/datatypeSummary.js
@@ -0,0 +1,22 @@
+// Primitive
+   
+    // 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+
+// Reference (Non primitive)
+   
+    // Array, Objects, Functions
+
+
+    const heros = ["a", "b"]
+
+    let myObj = {
+
+        name : "sam",
+        age:27,
+    }
+
+    const myFunction = function(){
+        console.log("hello world!!!")
+    }
