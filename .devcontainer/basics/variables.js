02b9aae09c204911855ade00b5869e669d958327
Abhishek Shukla <37998474+abhi107@users.noreply.github.com>
2023-10-14 07:35:16 +0000

    Variables in js

---
+++ .devcontainer/basics/variables.js
@@ -0,0 +1,16 @@
+const accountId = 9076534
+let accountEmail = "Abc@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 not allowed to change as it is constt
+
+accountEmail = "a@gmail.com"
+accountPassword = "1234567"
+accountCity = "Pune"
+
+console.log(accountId)
+console.table([accountEmail, accountPassword,accountCity,accountState])
+
+
