2183b681ed5a46e949ad8a7f8eafd441155a04a2
Abhishek Shukla <37998474+abhi107@users.noreply.github.com>
2023-10-20 16:25:52 +0000

    Arrays added

---
+++ .devcontainer/basics/09_Array.js
@@ -0,0 +1,27 @@
+// array
+
+const myArray = [1,2,3,4,5,6]
+const myArr2 = new Array(1,2,3,4)
+
+
+console.log(arr[1]);
+
+// Array methods
+
+myArr2.push(7)
+//myArr.pop()
+// myArr.unshift(9) -> to add at starting
+// myArr.shift()-> to del from starting
+// myArray.includes(9)
+// myArray.join() -> converts into string
+console.log(myArray)
+
+//slice --> Ignores last range & it doesn't manipulate orig array
+//splice --> Includes last range & it manipulates original array
+
+//concat --> It can merge two array
+//Spread --> It can merge any no. of array
+//flat(depth) -->return single array of sub array
+//Array.isArray()
+//Array.from()
+//Array.of()
